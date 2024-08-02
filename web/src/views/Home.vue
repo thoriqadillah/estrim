ae94536b3683d9adf244b61bad6849b9c0f1e53b
thoriqadillah <thoriqadillah59@gmail.com>
2024-08-02 21:45:28 +0700

    add shadcn

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <main>
     <p>Home</p>
-    <RouterLink to="/about">About</RouterLink>    
+    <RouterLink to="/about">
+      <Button>About</Button>
+    </RouterLink>    
   </main>
 </template>
